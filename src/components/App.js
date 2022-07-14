35d96aaed38be8c766528c307e7511465ef56749
Nathália <nathalia.gramst@hotmail.com>
2022-07-13 21:47:45 -0300

    Carregando arquivos básicos

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div>
+            {/* <Header />
+            <PageContent />
+            <Footer /> */}
+        </div>
+    );
+}
+
+// function Header() {}
+
+// function PageContent() {}
+
+// function Footer() {}
