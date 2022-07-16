cfe723e26eaffc0fa246d042d6ba16d7e9e5c2fd
Nathália <nathalia.gramst@hotmail.com>
2022-07-16 20:59:50 -0300

    Adicionando Like no botão do comentário

--- src/components/Main/Content/Posts/Postcomponents/Comments/Comment.js
+++ src/components/Main/Content/Posts/Postcomponents/Comments/Comment.js
@@ -1,8 +1,23 @@
+import React, { useState } from "react";
+
 export default function Comment(props) {
+    const [iconName, setIconName] = useState('heart-outline');
+    const [iconClass, setIconClass] = useState('heart-icon');
+
+    function toggleLike() {
+        if (iconName === 'heart-outline' && iconClass === 'heart-icon') {
+            setIconName('heart');
+            setIconClass('heart-icon liked');
+        } else {
+            setIconName('heart-outline');
+            setIconClass('heart-icon');
+        }
+    }
+
     return (
         <div className="comment">
             <p><span>{props.by}</span>  {props.text}</p>
-            <button type="button"><ion-icon name="heart-outline" class="heart-icon"></ion-icon></button>
+            <button type="button"><ion-icon name={iconName} class={iconClass} onClick={toggleLike}></ion-icon></button>
         </div>
     )
 }
