45b419d627587108a5f6d869c74f96fb34bd703c
Nathália <nathalia.gramst@hotmail.com>
2022-07-16 22:49:09 -0300

    Animação de coração na imagem

--- src/components/Main/Content/Posts/Postcomponents/Postimg.js
+++ src/components/Main/Content/Posts/Postcomponents/Postimg.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 export default function Postimg(props) {
+    const [imageHeart, setImageHeart] = useState('post-heart');
+
     function addLike() {
         if (props.iconName === 'heart-outline' && props.iconClass === 'heart-icon') {
             props.setIconName('heart');
             props.setIconClass('heart-icon liked');
+            setImageHeart('post-heart animation');
+        } else {
+            setImageHeart('post-heart');
         }
     }
 
     return (
         <div className="post-img">
             <img src={props.img} alt="" onClick={addLike} />
+            <div className={imageHeart}>
+            </div>
         </div>
     )
 }
